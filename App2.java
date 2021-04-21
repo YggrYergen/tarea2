8cd119b6a055cea45c3a5f041aa84fa6e6cc03fe
Tomas Gemes Salinas <tomasgemes@gmail.com>
2021-04-21 18:33:01 -0400

    avance menu

--- App2.java
+++ App2.java
@@ -2,7 +2,7 @@ package tarea2;
 
 public class App2 {
     static void dispMenu() {
-                "----------------------------------------------------------------------------------------------------------");
+        /*
         int opcion_1;
 
         System.out.println("Ingrese 1 para agregar libro.");
@@ -15,7 +15,7 @@ public class App2 {
         System.out.println("Ingrese 8 para agregar o quitar edificio.");
         System.out.println("Ingrese 9 para agregar o quitar sede.");
         System.out.println("Ingrese 0 para salir.");
-        "----------------------------------------------------------------------------------------------------------");
+        */
     }
 
     public static void main(String[] args) {
