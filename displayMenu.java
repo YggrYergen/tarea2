c3e06eb06b99bfcf6afb3308d9318c37b0d93deb
Tomas Gemes <tomasgemes@gmail.com>
2021-04-30 20:58:49 -0400

    displayMeny placeholder added

---
+++ displayMenu.java
@@ -0,0 +1,120 @@
+import java.io.*;
+
+public class displayMenu {
+
+    public static void dispMenu() {
+        int opcion_1;
+        int opcion_2;
+
+        System.out.println("Ingrese 1 para agregar libro.");
+        System.out.println("Ingrese 2 para editar libro.");
+        System.out.println("Ingrese 3 para buscar libro.");
+        System.out.println("Ingrese 4 para quitar libro.");
+        System.out.println("Ingrese 5 para agregar o quitar seccion.");
+        System.out.println("Ingrese 6 para agregar o quitar estante.");
+        System.out.println("Ingrese 7 para agregar o quitar piso.");
+        System.out.println("Ingrese 8 para agregar o quitar edificio.");
+        System.out.println("Ingrese 9 para agregar o quitar sede.");
+        System.out.println("Ingrese 0 para salir.");
+    
+        switch(opcion_1) {
+            case 1: // Agregar libro
+                // Methods.agregarLibro(etc)
+                break;
+
+            case 2: // Editar libro
+                //  // Desplegar opciones de edicion 
+                //    Cambiar titulo              Cata
+                //    Cambiar autor               Cata 
+                //    Cambiar anio                Cata
+                //    Cambiar estante_numero      Cata
+                //    Cambiar estante_seccion     Cata
+                //    Cambiar Piso                Cata
+                //    Cambiar Edificio            Cata
+                //    Cambiar Sede
+                switch(opcion_2) {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                }
+                break;
+
+            case 3:
+              // Buscar libro
+              // Methods.buscarLibro(etc)
+                break;
+
+            case 4:
+              // Quitar libro
+              // Methods.quitarLibro(etc)
+                break;
+
+            case 5:
+              // Agregar o quitar seccion
+              // Desplegar opciones agregar o quitar
+               
+              switch(opcion_2) {
+
+              }
+              break;
+
+            case 6:
+              // Agregar o quitar estante
+              // Desplegar opciones agregar o quitar
+               
+              switch(opcion_2) {
+
+              }
+              break;
+
+            case 7:
+              // Agregar o quitar piso
+              // Desplegar opciones agregar o quitar
+               
+              switch(opcion_2) {
+
+              }
+              break;
+
+            case 8:
+              // Agregar o quitar edificio
+              // Desplegar opciones agregar o quitar
+               
+              switch(opcion_2) {
+
+              }
+              break;
+
+            case 9:
+              // Agregar o quitar sede
+              // Desplegar opciones agregar o quitar
+               
+              switch(opcion_2) {
+
+              }
+              break;
+
+            case 0:
+              // Salir
+              // Desplegar opciones guardar o no
+               
+              switch(opcion_2) {
+
+              }
+              break;   
+          }
+          
+    }
