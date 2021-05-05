033ae8ff65b64fc2b9c50043cdf157b16ec9d079
Rodrigo Rojas <rod.rojas@alumnos.uai.cl>
2021-05-05 18:40:51 -0400

    aiura

--- DisplayMenu.java
+++ DisplayMenu.java
@@ -1,16 +1,14 @@
 import java.io.*;
 import java.util.Scanner;
 
-public class displayMenu {
+public class DisplayMenu {
 
-  public static void dispMenu() {
+  public static void dispMenu(Scanner input) {
     int opcion_menu = 0;
     int opcion_submenu = 0;
     boolean salir = false;
 
-    while(salir!=true) {
-
-      Scanner scan = new Scanner(System.in);
+    while (salir != true) {
 
       System.out.println("Ingrese 1 para agregar libro.");
       System.out.println("Ingrese 2 para editar libro.");
@@ -23,17 +21,13 @@ public class displayMenu {
       System.out.println("Ingrese 9 para agregar o quitar sede.");
       System.out.println("Ingrese 0 para salir.");
 
-      opcion_menu = scan.nextInt();
+      opcion_menu = input.nextInt();
 
-      
-
-
-      switch(opcion_menu) {
+      switch (opcion_menu) {
         case 1: // Agregar libro
           // Methods.agregarLibro(etc);
           break;
 
-    
         case 2: // Editar libro
           System.out.println("Ingrese 1 para cambiar titulo.");
           System.out.println("Ingrese 2 para cambiar autor.");
@@ -44,38 +38,38 @@ public class displayMenu {
           System.out.println("Ingrese 7 para cambiar edifico.");
           System.out.println("Ingrese 8 para cambiar sede.");
 
-          opcion_submenu = scan.nextInt();
+          opcion_submenu = input.nextInt();
 
-          switch(opcion_submenu) {
-            case 1: //    Cambiar titulo
+          switch (opcion_submenu) {
+            case 1: // Cambiar titulo
               // method.cambiarTitulo(etc)
               break;
 
-            case 2: //    Cambiar autor
+            case 2: // Cambiar autor
               // method.cambiarTitulo(etc)
               break;
 
-            case 3: //    Cambiar anio
+            case 3: // Cambiar anio
               // method.cambiarAnio(etc)
               break;
 
-            case 4: //    Cambiar estante_numero
+            case 4: // Cambiar estante_numero
               // method.cambiarEstante(etc)
               break;
 
-            case 5: //    Cambiar estante_seccion
+            case 5: // Cambiar estante_seccion
               // method.cambiarSeccion(etc)
               break;
 
-            case 6:  //    Cambiar Piso
+            case 6: // Cambiar Piso
               // method.cambiarPiso(etc)
               break;
 
-            case 7: //    Cambiar Edificio
+            case 7: // Cambiar Edificio
               // method.cambiarEdificio(etc)
               break;
 
-            case 8: //    Cambiar Sede
+            case 8: // Cambiar Sede
               // method.cambiarSede(etc)
               break;
           }
@@ -92,10 +86,10 @@ public class displayMenu {
         case 5: // Agregar o quitar seccion
           System.out.println("Ingrese 1 para agregar seccion.");
           System.out.println("Ingrese 2 para quitar seccion.");
-          
-          opcion_submenu = scan.nextInt();
 
-          switch(opcion_submenu) {
+          opcion_submenu = input.nextInt();
+
+          switch (opcion_submenu) {
             case 1:
               // Methods.agregarSeccion(etc)
               break;
@@ -109,10 +103,10 @@ public class displayMenu {
         case 6: // Agregar o quitar estante
           System.out.println("Ingrese 1 para agregar estante.");
           System.out.println("Ingrese 2 para quitar estante.");
-          
-          opcion_submenu = scan.nextInt();
 
-          switch(opcion_submenu) {
+          opcion_submenu = input.nextInt();
+
+          switch (opcion_submenu) {
             case 1:
               // Methods.agregarEstante(etc)
               break;
@@ -126,12 +120,12 @@ public class displayMenu {
         case 7: // Agregar o quitar piso
           System.out.println("Ingrese 1 para agregar piso.");
           System.out.println("Ingrese 2 para quitar piso.");
-          
-          opcion_submenu = scan.nextInt();
 
-          switch(opcion_submenu) {
+          opcion_submenu = input.nextInt();
+
+          switch (opcion_submenu) {
             case 1:
-              //Methods.agregarPiso()
+              // Methods.agregarPiso()
               break;
 
             case 2:
@@ -141,12 +135,12 @@ public class displayMenu {
           break;
 
         case 8: // Agregar o quitar edificio
-        System.out.println("Ingrese 1 para agregar edificio.");
-        System.out.println("Ingrese 2 para quitar quitar piso.");
-          
-          opcion_submenu = scan.nextInt();
+          System.out.println("Ingrese 1 para agregar edificio.");
+          System.out.println("Ingrese 2 para quitar quitar piso.");
 
-          switch(opcion_submenu) {
+          opcion_submenu = input.nextInt();
+
+          switch (opcion_submenu) {
             case 1:
               // Methods.agregarEdificio();
               break;
@@ -158,12 +152,12 @@ public class displayMenu {
           break;
 
         case 9: // Agregar o quitar sede
-        System.out.println("Ingrese 1 para agregar sede.");
-        System.out.println("Ingrese 2 para quitar sede.");
-          
-          opcion_submenu = scan.nextInt();
+          System.out.println("Ingrese 1 para agregar sede.");
+          System.out.println("Ingrese 2 para quitar sede.");
 
-          switch(opcion_submenu) {
+          opcion_submenu = input.nextInt();
+
+          switch (opcion_submenu) {
             case 1:
               // Methods.agregarSede();
               break;
@@ -175,12 +169,12 @@ public class displayMenu {
           break;
 
         case 0: // Salir con o sin guardar
-        System.out.println("Ingrese 1 para salir y no guardar .");
-        System.out.println("Ingrese 2 para salir y guardar .");
-          
-          opcion_submenu = scan.nextInt();
+          System.out.println("Ingrese 1 para salir y no guardar .");
+          System.out.println("Ingrese 2 para salir y guardar .");
 
-          switch(opcion_submenu) {
+          opcion_submenu = input.nextInt();
+
+          switch (opcion_submenu) {
             case 1:
               // Methods.salirYguardar()
               break;
@@ -189,12 +183,10 @@ public class displayMenu {
               // Methods.salirSinGuardar()
               break;
           }
-          break; 
+          break;
       }
 
     }
 
-    
-  }      
+  }
 }
-
