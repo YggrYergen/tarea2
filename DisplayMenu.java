49cf8dbca3a4d228adab9c05cb271cdaeecc8ba4
CataCordova <80425504+CataCordova@users.noreply.github.com>
2021-05-14 00:58:17 -0400

    Update DisplayMenu.java

--- DisplayMenu.java
+++ DisplayMenu.java
@@ -3,12 +3,17 @@ import java.io.*;
 
 public class DisplayMenu {
 
+<<<<<<< Updated upstream
   public static void dispMenu(Biblioteca biblioteca) throws IOException {
+=======
+  public static void dispMenu(Sede sedes) {
+>>>>>>> Stashed changes
     Scanner input = new Scanner(System.in);
     boolean salir = false;
     int opcion_menu, opcion_submenu;
     Book book;
     while (salir != true) {
+
       System.out.println("\n¡Bienvenido a la biblioteca UAI!\n");
       System.out.println("Eliga una de las siguientes opciones:\n");
       System.out.println("[1] Agregar libro.");
@@ -16,13 +21,15 @@ public class DisplayMenu {
       System.out.println("[3] Buscar libro.");
       System.out.println("[4] Quitar libro.");
       System.out.println("[5] Agregar o quitar seccion.");
-      System.out.println("[6] Agregar o quitar estante.");
+      System.out.println("[6]] Agregar o quitar estante.");
       System.out.println("[7] Agregar o quitar piso.");
       System.out.println("[8] Agregar o quitar edificio.");
       System.out.println("[9] Agregar o quitar sede.");
       System.out.println("[0] Salir.");
       System.out.print("\nOpcion:  ");
+
       opcion_menu = input.nextInt();
+
       switch (opcion_menu) {
         case 1: // Agregar libro
           input.nextLine();
@@ -36,7 +43,9 @@ public class DisplayMenu {
 
           //////////////////
           break;
+
         case 2: // Editar libro
+<<<<<<< Updated upstream
           input.nextLine();
           System.out.print("\033[H\033[2J");
           System.out.println("\nIngresar Libro a editar:");
@@ -101,12 +110,19 @@ public class DisplayMenu {
               // method.cambiarSede(etc)
               break;
           }
+=======
+          System.out.print("\033[H\033[2J");
+          System.out.println("Ingresar Titulo a editar:");
+          String titulo = input.nextLine();
+          books.editBook(input);
+>>>>>>> Stashed changes
           break;
 
         case 3: // Buscar libro
           input.nextLine();
           System.out.print("\033[H\033[2J");
           System.out.println("Ingresar Titulo a buscar:");
+<<<<<<< Updated upstream
           titulo = input.nextLine();
           String bookInfo = biblioteca.buscarTitulo(titulo);
           if (bookInfo != null) {
@@ -116,112 +132,107 @@ public class DisplayMenu {
             System.out.println(("\nPresione enter para continuar."));
             input.nextLine();
           }
+=======
+          String titulo = input.nextLine();
+          Book book = books.buscarTitulo(titulo);
+          System.out.println(book.toString());
+>>>>>>> Stashed changes
           break;
 
         case 4: // Quitar libro
-          System.out.print("\033[H\033[2J");
-          System.out.println("Ingresar Titulo a Eliminar:");
-          // String titulo = input.nextLine();
-          // Book book = books.eliminarTitulo(titulo);
+          // Methods.quitarLibro(etc)
           break;
+
         case 5: // Agregar o quitar seccion
-          System.out.print("\033[H\033[2J");
-          System.out.println("Eliga una de las siguientes opciones:\n");
-          System.out.println("\n[1] Agregar seccion.");
-          System.out.println("[2] Quitar seccion.");
-          System.out.print("\nOpcion:  ");
-          opcion_submenu = input.nextInt();
-          switch (opcion_submenu) {
-            case 1:
-              // Methods.agregarSeccion(etc)
-              break;
 
-            case 2:
-              // Methods.quitarSeccion(etc)
-              break;
-          }
+          // funcion;
+
+          /*
+           * System.out.println("Ingrese 1 para agregar seccion.");
+           * System.out.println("Ingrese 2 para quitar seccion."); opcion_submenu =
+           * input.nextInt();
+           */
+
           break;
+
         case 6: // Agregar o quitar estante
-          System.out.print("\033[H\033[2J");
-          System.out.println("Eliga una de las siguientes opciones:\n");
-          System.out.println("\n[1] Agregar estante.");
-          System.out.println("[2] Quitar estante.");
-          System.out.print("\nOpcion:  ");
-          opcion_submenu = input.nextInt();
-          switch (opcion_submenu) {
-            case 1:
-              // Methods.agregarEstante(etc)
-              break;
 
-            case 2:
-              // Methods.quitarEstante(etc)
-              break;
-          }
+          // funcion;
+
+          /*
+           * System.out.println("Ingrese 1 para agregar estante.");
+           * System.out.println("Ingrese 2 para quitar estante.");
+           * 
+           * opcion_submenu = input.nextInt();
+           * 
+           * switch (opcion_submenu) { case 1: // Methods.agregarEstante(etc) break;
+           * 
+           * case 2: // Methods.quitarEstante(etc) break; }
+           */
 
           break;
+
         case 7: // Agregar o quitar piso
-          System.out.print("\033[H\033[2J");
-          System.out.println("Eliga una de las siguientes opciones:\n");
-          System.out.println("\n[1] Agregar piso.");
-          System.out.println("[2] Quitar piso.");
-          System.out.print("\nOpcion:  ");
-          opcion_submenu = input.nextInt();
-          switch (opcion_submenu) {
-            case 1:
-              // Methods.agregarPiso()
-              break;
 
-            case 2:
-              // Methods.quitarPiso()
-              break;
-          }
+          // funcion;
+
+          /*
+           * System.out.println("Ingrese 1 para agregar piso.");
+           * System.out.println("Ingrese 2 para quitar piso.");
+           * 
+           * opcion_submenu = input.nextInt();
+           * 
+           * switch (opcion_submenu) { case 1: Methods.agregarPiso() break;
+           * 
+           * case 2: Methods.quitarPiso() break; }
+           */
 
           break;
+
         case 8: // Agregar o quitar edificio
-          System.out.print("\033[H\033[2J");
-          System.out.println("Eliga una de las siguientes opciones:\n");
-          System.out.println("\nI[1] Agregar edificio.");
-          System.out.println("[2] Quitar quitar piso.");
-          System.out.print("\nOpcion:  ");
-          opcion_submenu = input.nextInt();
-          switch (opcion_submenu) {
-            case 1:
-              // Methods.agregarEdificio();
-              break;
 
-            case 2:
-              // Methods.quitarEdificio();
-              break;
-          }
+          // funcion;
+
+          /*
+           * System.out.println("Ingrese 1 para agregar edificio.");
+           * System.out.println("Ingrese 2 para quitar quitar piso.");
+           * 
+           * opcion_submenu = input.nextInt();
+           * 
+           * switch (opcion_submenu) { case 1: Methods.agregarEdificio(); break;
+           * 
+           * case 2: Methods.quitarEdificio(); break; }
+           */
 
           break;
+
         case 9: // Agregar o quitar sede
-          System.out.print("\033[H\033[2J");
-          System.out.println("Eliga una de las siguientes opciones:\n");
-          System.out.println("\n[1] Agregar sede.");
-          System.out.println("[2] Quitar sede.");
-          System.out.print("\nOpcion:  ");
-          opcion_submenu = input.nextInt();
-          switch (opcion_submenu) {
-            case 1:
-              // Methods.agregarSede();
-              break;
+          // funcion;
 
-            case 2:
-              // Methods.quitarSede();
-              break;
-          }
+          /*
+           * System.out.println("Ingrese 1 para agregar sede.");
+           * System.out.println("Ingrese 2 para quitar sede.");
+           * 
+           * opcion_submenu = input.nextInt();
+           * 
+           * switch (opcion_submenu) { case 1: Methods.agregarSede(); break;
+           * 
+           * case 2: Methods.quitarSede(); break; }
+           */
 
           break;
+
         case 0: // Salir
           System.out.print("\033[H\033[2J");
           System.out.println("Guardando y saliendo...\n");
           // Sistema de guardado //
           Methods.writeCSV(biblioteca);
           salir = true;
+
           break;
       }
+
     }
-    input.close();
+
   }
 }
